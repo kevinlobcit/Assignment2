3b9b91b911b5fc43aa60ba4bb0cdbb8594b87e8e
Owntrolf <klo74@my.bcit.ca>
2018-11-23 13:50:10 -0800

    Initial commit

---
+++ Tour.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Owntrolf on 2018-11-23.
+//
+
+#include "Tour.hpp"
---
+++ Tour.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Owntrolf on 2018-11-23.
+//
+
+#ifndef ASSIGNMENT_TOUR_HPP
+#define ASSIGNMENT_TOUR_HPP
+
+
+class Tour {
+
+};
+
+
+#endif //ASSIGNMENT_TOUR_HPP
